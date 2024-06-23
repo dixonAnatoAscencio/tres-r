1d8cf46df32864a79f9769223b592c285024511b
dixonAnatoAscencio <dixonjesusanato@gmail.com>
2024-06-23 02:35:35 -0500

    estados asincronos

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const TURNS = {
+  X: "x",
+  O: "o",
+};
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  //componente separado de la app reutilizable
+  const className = `square ${isSelected ? "is-selected" : ""}`;
+
+  const handleClick = () => {
+    updateBoard(index);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+     <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  );
+};
+
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
+
+function App() { //componente padre app
+  const [board, setBoard] = useState(Array(9).fill(null));
+  console.log(board);
+
+  const [turn, setTurn] = useState(TURNS.X); //inicie el turno en X
+  const [winner, setWinner] = useState(null); //null es que no hay ganador , false es que hay un empate
+
+  const checkWinner = (boardToCheck) => {
+    for(const combo of WINNER_COMBOS) {
+      //revisamos todas las combinaciones de 3 posiciones
+      //si todas las posiciones son iguales, es un ganador
+      const [a, b, c] = combo;
+      if(
+        boardToCheck[a] && boardToCheck[a] === boardToCheck[b] && boardToCheck[a] === boardToCheck[c]
+      ){
+        return boardToCheck[a];
+      }
+    }
+    return null; //no hay ganador
+  }
+
+  const updateBoard = (index) => {
+    if(board[index] || winner) return; // cunado en esa posicion ya esta marcado o hay ganador no se actualiza
+    //actualizamos el tablero
+    const newBoard = [...board]//crear una copia del array // nunca hacer esto board[index] = turn no tenemos que mutar nunca las props ni el ESTADO
+    newBoard[index] = turn;
+    setBoard(newBoard);
+
+    //actualizamos el turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+    //revisar si hay ganador
+    const newWinner = checkWinner(board);
+   if(newWinner) {
+     setWinner(newWinner);//los estados son asincronos, por eso debemos pasar siempre el ultimo estado. IMPORTANTISIMO
+   }
+  }
+
+  return (
+    <main className="board">
+      <h1>Tic Tac Toe</h1>
+      <section className="game">
+        {board.map((_, index) => {
+          return (
+            <Square key={index} index={index} updateBoard={updateBoard} >
+              {board[index]}
+            </Square>
+          );
+        })}
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
